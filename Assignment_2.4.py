5cff2f196fc7a7e4f5fc75da96b465b61d09297e
Naresh Muthyala <nareshmuthyala1@gmail.com>
2018-03-01 08:08:54 +0530

    Assignment_2.4.py

---
+++ Assignment_2.4.py
@@ -0,0 +1,2 @@
+string = "WE, THE PEOPLE OF INDIA,\n\thaving solemnly resolved to constitute India into a SOVEREIGN, !\n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC \n\t\t and to secure to all its citizens:"
+print(string)
